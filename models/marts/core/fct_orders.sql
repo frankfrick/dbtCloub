f7eae8defcb5ede3825a55e12a2097f9cc3afd37
frankfrick@gmail.com <frankfrick@gmail.com>
2022-10-21 12:49:18 +0000

    models demo

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,4 +1,30 @@
-select so.order_id, dc.customer_id, sp.amount
-from {{ ref('dim_customers')}} dc
-join {{ ref('stg_orders')}} so on so.customer_id = dc.customer_id
-join {{ ref('stg_payments')}} sp on sp.order_id = so.order_id
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
